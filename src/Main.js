cb6b59a0b8e172e017591f6fa858701eb84f9433
Alissa Ovalle Marquez <alissa.ovalle@gmail.com>
2019-10-23 16:54:13 -0500

    sandbox folder and main stylesheet created

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,11 @@
 import React, {Component} from "react"
-import List from './List'
 import Title from './Title'
 
 class Main extends Component {
     render(){
         return(
             <div>
-                <Title title = {'Morning Routine: Part 1'}/>
-                <List morningTasks = {['Put on mascara', 'Cuddle the cat']}/>
-                <Title title = {'Morning Routine: Part 2'}/>
-                <List morningTasks = {['Brush teeth', 'Fix hair', 'Put on clothes', 'Feed cat', 'Pack lunch']}/>
+                <Title title = {'Photowall'}/>
             </div>
         )
     }
